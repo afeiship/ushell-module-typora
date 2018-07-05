466e960614e1cd5ac284c239b36760ff91de8475
afeiship <1290657123@qq.com>
2018-07-05 18:50:54 +0800

    wip

--- install-theme.sh
+++ install-theme.sh
@@ -3,4 +3,4 @@
 TYPORA_THEME_LINK=$1;
 TYPORA_THEME_DIR="~/Library/Application\ Support/abnerworks.Typora/themes";
 
-wget $TYPORA_THEME_LINK -P TYPORA_THEME_DIR;
+wget $TYPORA_THEME_LINK -P $TYPORA_THEME_DIR;
