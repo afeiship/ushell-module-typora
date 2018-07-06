a0e9ff096de615f71337dfecf6d0ab84bad18f34
afeiship <1290657123@qq.com>
2018-07-06 09:20:36 +0800

    wip

--- index.sh
+++ index.sh
@@ -2,8 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias typora="open -a typora";
-alias typo="open -a typora";
-alias typo-install-theme="${ROOT_PATH}/install-theme.sh";
+alias tpo="open -a typora";
+alias tpo-install-theme="${ROOT_PATH}/install-theme.sh";
 
 unset ROOT_PATH;
